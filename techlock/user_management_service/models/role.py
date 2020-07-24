26bf91606e711d1a6bb661bad14d7c845f5b45f1
Michiel Vanderlee <jmt.vanderlee@gmail.com>
2020-07-24 00:52:43 -0300

    fix: add stage to  role idp_name

--- techlock/user_management_service/models/role.py
+++ techlock/user_management_service/models/role.py
@@ -1,5 +1,6 @@
 import marshmallow as ma
 import marshmallow.fields as mf
+import os
 from dataclasses import dataclass
 from sqlalchemy.dialects.postgresql import JSONB
 
@@ -33,6 +34,7 @@ ROLE_CLAIM_SPEC = ClaimSpec(
     resource_name='roles',
     filter_fields=[]
 )
+STAGE = os.environ.get('STAGE', 'dev').upper()
 
 
 class RoleSchema(BaseModelSchema):
@@ -62,7 +64,7 @@ class Role(BaseModel):
 
     @property
     def idp_name(self):
-        return f'{self.tenant_id}_{self.name}'
+        return f'{self.tenant_id}_{STAGE}_{self.name}'
 
 
 @dataclass
