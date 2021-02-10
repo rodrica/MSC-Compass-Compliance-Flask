ecd735cd857fe44b6de52e43c5a8fb93c5faf401
Michiel Vanderlee <jmt.vanderlee@gmail.com>
2021-02-10 11:43:09 -0400

    fix cryptography build issue

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     author_email='jmt.vanderlee@gmail.com',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
-        'tl_msc_common==1.0.0.dev0+master.15170e44893ec4be9d5edd4df86b0b2af7ce3ac3',
+        'tl_msc_common==1.0.0.dev0+master.7560c6db82a32987ca2b6cbafa3efde3844df3c4',
         'Flask-HTTPAuth==3.3.0',
         'flask-smorest==0.19.2',
     ],
