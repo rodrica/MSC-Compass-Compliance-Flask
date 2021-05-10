00d58e39f0c19b6ddac35acb1d74007ccb0e1be4
Michiel Vanderlee <jmt.vanderlee@gmail.com>
2021-05-10 18:51:44 -0300

    update hydrator schema

--- techlock/user_management_service/models/hydrator.py
+++ techlock/user_management_service/models/hydrator.py
@@ -1,5 +1,6 @@
 import marshmallow as ma
 import marshmallow.fields as mf
+from marshmallow.utils import EXCLUDE
 
 __all__ = [
     'HydratorPostSchema',
@@ -7,9 +8,14 @@ __all__ = [
 
 
 class HydratorPostSchema(ma.Schema):
+    '''
+        https://www.ory.sh/oathkeeper/docs/pipeline/mutator/#hydrator
+    '''
     class Meta:
-        ordered = True
+        ordered = True,
+        unknown = EXCLUDE
 
     subject = mf.String(allow_none=True)
     extra = mf.Dict(keys=mf.String(), allow_none=True)
     header = mf.Dict(keys=mf.String(), allow_none=True)
+    match_context = mf.Dict(keys=mf.String(), allow_none=True)
