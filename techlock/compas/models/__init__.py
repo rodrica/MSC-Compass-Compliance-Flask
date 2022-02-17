7f3b90307e8108c98b63a75fce1c03447c9a39fc
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2022-02-17 14:18:41 +0500

    Removed legacy

--- techlock/compas/models/__init__.py
+++ techlock/compas/models/__init__.py
@@ -1,25 +1,12 @@
-from .hydrator import HydratorPostSchema
-from .role import (
-    ROLE_CLAIM_SPEC,
-    Role,
-    RoleListQueryParameters,
-    RoleListQueryParametersSchema,
-    RolePageableSchema,
-    RoleSchema,
-)
-from .ui_data import DashboardDataSchema
-from .user import (
-    USER_CLAIM_SPEC,
-    PostUserChangePasswordSchema,
-    UpdateUserSchema,
-    User,
-    UserListQueryParameters,
-    UserListQueryParametersSchema,
-    UserPageableSchema,
-    UserSchema,
+from .report import (
+    REPORT_CLAIM_SPEC,
+    Report,
+    ReportListQueryParameters,
+    ReportListQueryParametersSchema,
+    ReportPageableSchema,
+    ReportSchema,
 )
 
 ALL_CLAIM_SPECS = [
-    ROLE_CLAIM_SPEC,
-    USER_CLAIM_SPEC,
+    REPORT_CLAIM_SPEC
 ]
