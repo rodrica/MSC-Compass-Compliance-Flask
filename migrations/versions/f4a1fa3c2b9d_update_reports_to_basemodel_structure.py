bc357fa314fdf94a6027f0dcc071dab80be34e84
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2022-02-17 15:33:35 +0500

    Added migration reports to BaseModel conformance

--- migrations/versions/f4a1fa3c2b9d_update_reports_to_basemodel_structure.py
+++ migrations/versions/f4a1fa3c2b9d_update_reports_to_basemodel_structure.py
@@ -6,7 +6,9 @@ Create Date: 2022-02-17 14:34:59.538319
 
 """
 from alembic import op
+from jinja2 import defaults
 import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
 
 
 # revision identifiers, used by Alembic.
@@ -17,8 +19,26 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    with op.batch_alter_table("reports", "public") as t:
+        t.add_column(sa.Column("description", sa.String, unique=False, nullable=True))
+        t.add_column(sa.Column("tags", postgresql.JSONB, nullable=True))
+
+        t.add_column(sa.Column("tenant_id", sa.String, unique=False, nullable=False, server_default="SYSTEM"))
+        t.add_column(sa.Column("created_by", sa.String, unique=False, nullable=True))
+        t.add_column(sa.Column("created_on", sa.DateTime, unique=False, nullable=True))
+        t.add_column(sa.Column("changed_by", sa.String, unique=False, nullable=True))
+        t.add_column(sa.Column("changed_on", sa.DateTime, unique=False, nullable=True))
+        t.add_column(sa.Column("is_active", sa.Boolean, unique=False, nullable=False, server_default="TRUE"))
 
 
 def downgrade():
-    pass
+    with op.batch_alter_table("reports", "public") as t:
+        t.drop_column("description")
+        t.drop_column("tags")
+
+        t.drop_column("tenant_id")
+        t.drop_column("created_by")
+        t.drop_column("created_on")
+        t.drop_column("changed_by")
+        t.drop_column("changed_on")
+        t.drop_column("is_active")
