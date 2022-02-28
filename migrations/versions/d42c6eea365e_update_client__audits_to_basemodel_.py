a41d6fdd638b12c86b97b7e3f93c357e05438391
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2022-02-28 15:21:23 +0500

    Audit migration updated to move table from client schema to public

--- migrations/versions/d42c6eea365e_update_client__audits_to_basemodel_.py
+++ migrations/versions/d42c6eea365e_update_client__audits_to_basemodel_.py
@@ -8,6 +8,12 @@ Create Date: 2022-02-25 13:16:06.825951
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
+from sqlalchemy.sql.schema import Column
+from yaml.events import ScalarEvent
+
+from techlock.compas.models.audit import Phase
+
+from techlock.compas.models.int_enum import IntEnum
 
 
 
@@ -22,36 +28,93 @@ def upgrade():
     engine = op.get_bind()
     ins = sa.inspect(engine)
     schemas = ins.get_schema_names()
+
+    op.create_table("audits", 
+                    sa.Column('id', sa.Integer, primary_key=True),
+                    # id from table in client schema for migration
+                    sa.Column('internal_id', sa.Integer),
+
+                    sa.Column("name", sa.String, unique=False, nullable=False, server_default=""),
+                    sa.Column("description", sa.String, unique=False, nullable=True),
+                    sa.Column("tags", postgresql.JSONB, nullable=True),
+
+                    sa.Column("tenant_id", sa.String, unique=False,
+                              nullable=False),
+                    sa.Column("created_by", sa.String, unique=False, nullable=True),
+                    sa.Column("changed_by", sa.String, unique=False, nullable=True),
+                    sa.Column("created_on", sa.DateTime, unique=False, nullable=True),
+                    sa.Column("changed_on", sa.DateTime, unique=False, nullable=True),
+                    sa.Column("is_active", sa.Boolean, unique=False, nullable=False, server_default="TRUE"),
+                    sa.Column("user_id", sa.String, nullable=False),
+                    sa.Column("reports", postgresql.ARRAY(sa.Integer),
+                              nullable=False),
+                    sa.Column("start_date", sa.Date, nullable=False),
+                    sa.Column("estimated_remediation_date", sa.Date),
+                    sa.Column("remediation_date", sa.Date),
+                    sa.Column("estimated_end_date", sa.Date,
+                              nullable=False),
+                    sa.Column("end_date", sa.Date),
+                    sa.Column("phase", IntEnum(Phase),
+                              default=Phase.scoping_and_validation),
+                    schema="public")
     for schema in schemas:
         if schema not in ('public', 'information_schema'):
-            op.execute('UPDATE "{}"."{}" SET deleted=not deleted'.format(schema, "audits"))
-            with op.batch_alter_table("audits", schema) as t:
-                t.add_column(sa.Column("description", sa.String, unique=False, nullable=True))
-                t.add_column(sa.Column("tags", postgresql.JSONB, nullable=True))
+            op.execute(
+                '''INSERT INTO "public"."audits"(internal_id, name, reports,
+                estimated_end_date,
+                start_date, estimated_remediation_date,
+                remediation_date, end_date, phase, user_id, tenant_id,
+                is_active, created_on, changed_on)
+            SELECT id, name, reports, estimated_end_date,
+                start_date, estimated_remediation_date,
+                remediation_date, end_date, phase, user_id, '{}', not deleted,
+                inserted_at, updated_at FROM "{}"."audits"
+            '''.format(schema, schema)
+            )
 
-                t.add_column(sa.Column("tenant_id", sa.String, unique=False,
-                                       nullable=False, server_default=schema))
-                t.add_column(sa.Column("created_by", sa.String, unique=False, nullable=True))
-                t.alter_column("inserted_at", new_column_name="created_on")
-                t.alter_column("updated_at", new_column_name="changed_on")
-                t.add_column(sa.Column("changed_by", sa.String, unique=False, nullable=True))
-                t.alter_column("deleted", new_column_name="is_active")
+            # TODO: Create a schemas clear migration
+            # op.drop_table("audits", schema=schema)
 
 
 def downgrade():
-    engine = op.get_bind()
-    ins = sa.inspect(engine)
-    schemas = ins.get_schema_names()
-    for schema in schemas:
-        if schema not in ('public', 'information_schema'):
-            with op.batch_alter_table("audits", schema) as t:
-                t.drop_column("description")
-                t.drop_column("tags")
+    op.drop_table("audits", "public")
+    # TODO: probably clear schemas rollback, not here
+    # engine = op.get_bind()
+    # ins = sa.inspect(engine)
+    # schemas = ins.get_schema_names()
+    # for schema in schemas:
+    #     if schema not in ('public', 'information_schema'):
+    #         op.create_table("audits", 
+    #                         sa.Column('id', sa.Integer, primary_key=True),
+    #                         sa.Column("name", sa.String, unique=False, nullable=False, server_default=""),
 
-                t.drop_column("tenant_id")
-                t.drop_column("created_by")
-                t.alter_column("created_on", new_column_name="inserted_at")
-                t.alter_column("changed_on", new_column_name="updated_at")
-                t.drop_column("changed_by")
-                t.alter_column("is_active", new_column_name="deleted")
-            op.execute('UPDATE "{}"."{}" SET deleted=not deleted'.format(schema, "audits"))
+    #                         sa.Column("inserted_at", sa.DateTime, unique=False, nullable=True),
+    #                         sa.Column("updated_at", sa.DateTime, unique=False, nullable=True),
+    #                         sa.Column("deleted", sa.Boolean, unique=False, nullable=False, server_default="TRUE"),
+    #                         sa.Column("user_id", sa.String, nullable=False),
+    #                         sa.Column("reports", postgresql.ARRAY(sa.Integer),
+    #                                   nullable=False),
+    #                         sa.Column("start_date", sa.Date, nullable=False),
+    #                         sa.Column("estimated_remediation_date", sa.Date),
+    #                         sa.Column("remediation_date", sa.Date),
+    #                         sa.Column("estimated_end_date", sa.Date,
+    #                                   nullable=False),
+    #                         sa.Column("end_date", sa.Date),
+    #                         sa.Column("phase", IntEnum(Phase),
+    #                                   default=Phase.scoping_and_validation),
+    #                         schema=schema)
+    #         #op.execute(
+    #         #    '''INSERT INTO "{}"."audits"(id, name, reports,
+    #         #    estimated_end_date,
+    #         #    start_date, estimated_remediation_date,
+    #         #    remediation_date, end_date, phase, user_id,
+    #         #     deleted, inserted_at, updated_at)
+    #         #SELECT internal_id, name, reports, estimated_end_date,
+    #         #    start_date, estimated_remediation_date,
+    #         #    remediation_date, end_date, phase, user_id,
+    #         #    not is_active, created_on, changed_on
+    #         #    FROM "public"."audits"
+    #         #    WHERE tenant_id={}
+    #         #'''.format(schema, schema)
+    #         #)
+    #         pass
