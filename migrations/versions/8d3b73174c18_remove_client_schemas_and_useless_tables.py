f7e19d973db8ba2260a1eec944d1820113fb0719
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2022-04-15 15:49:58 +0500

    Added DB cleanup migration

---
+++ migrations/versions/8d3b73174c18_remove_client_schemas_and_useless_tables.py
@@ -0,0 +1,33 @@
+"""Remove client schemas and useless tables
+
+Revision ID: 8d3b73174c18
+Revises: 9c011b685c07
+Create Date: 2022-04-15 15:15:56.954154
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8d3b73174c18'
+down_revision = '9c011b685c07'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    engine = op.get_bind()
+    ins = sa.inspect(engine)
+    schemas = ins.get_schema_names()
+
+    for schema in schemas:
+        if schema not in ('public', 'information_schema'):
+            with op.get_context().autocommit_block():
+                op.execute("DROP SCHEMA {} CASCADE;".format(schema))
+
+    for table in ('user_global_permissions', 'schema_migrations', 'users'):
+        op.drop_table(table)
+
+def downgrade():
+    pass
